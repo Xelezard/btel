5c2ffbaa012536fb89d10bcaf5ff16a7d07d1a15
Xelezard <seidler.alexander@gmx.de>
2025-01-03 11:46:21 +0100

    Added Cursor

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use tui::{
     backend::CrosstermBackend, layout::{Constraint, Direction, Layout}, style::Style, text::{Span, Spans}, widgets::{Block, Borders, Paragraph, Widget}, Frame, Terminal
 };
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    cursor, event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode}, execute, terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
 };
 #[derive(Clone, Copy,Debug)]
 enum Mode {
@@ -20,7 +18,6 @@ struct App<'a>{
     input: &'a String,
     command: &'a String
 }
-
 fn main() -> Result<(), io::Error> {
     // setup terminal
     enable_raw_mode()?;
@@ -30,7 +27,6 @@ fn main() -> Result<(), io::Error> {
     let mut terminal = Terminal::new(backend)?;
 
     run(&mut terminal);
-    println!("SUS");
 
     // restore terminal
     disable_raw_mode()?;
@@ -47,8 +43,9 @@ fn run(terminal:&mut Terminal<CrosstermBackend<io::Stdout>>) -> Result<(),Error>
     let mut input = String::new();
     let mut mode = Mode::Mode;
     let mut command = String::new();
+    let mut edit_cursor:usize = 0;
     loop {
-        terminal.draw(|f|render(f, App {mode: mode,input: &input,command: &command}));
+        terminal.draw(|f|render(f, App {mode: mode,input: &input,command: &command},&mut edit_cursor));
         if let Event::Key(key) = event::read().unwrap() {
             match mode {
                 Mode::Mode => {
@@ -60,10 +57,13 @@ fn run(terminal:&mut Terminal<CrosstermBackend<io::Stdout>>) -> Result<(),Error>
                     }
                 },
                 Mode::Edit => match key.code {
-                    KeyCode::Char(x) => input += &x.to_string(),
-                    KeyCode::Backspace => {let _ = input.pop();},
-                    KeyCode::Enter => input += &"\n".to_string(),
-                    KeyCode::Esc => mode = Mode::Mode,
+                    KeyCode::Char(x) => {input.insert(edit_cursor, x)/*input += &x.to_string()*/;edit_cursor += 1;},
+                    KeyCode::Backspace => {let x = input.remove(edit_cursor);if x == '\n' {edit_cursor;};edit_cursor -= 1;},
+                    KeyCode::Enter => {input.insert(edit_cursor, '\n')/*input += &"\n".to_string()*/;edit_cursor;edit_cursor += 1;},
+                    KeyCode::Esc => {mode = Mode::Mode},
+                    KeyCode::Left => edit_cursor -= 1,
+                    KeyCode::Right => edit_cursor +=1,
+                    //KeyCode::Up => edit_cursor,
                     _ => ()
                 },
                 Mode::Error => {command = String::from("No mode found with this name");mode = Mode::Mode},
@@ -72,7 +72,7 @@ fn run(terminal:&mut Terminal<CrosstermBackend<io::Stdout>>) -> Result<(),Error>
         }
     }
 }
-fn render(f:&mut  Frame<'_,CrosstermBackend<io::Stdout>>, app: App) {
+fn render(f:&mut  Frame<'_,CrosstermBackend<io::Stdout>>, app: App,edit_cursor:&mut usize) {
     let chunks = Layout::default()
     .direction(Direction::Vertical)
     .margin(1)
@@ -84,18 +84,30 @@ fn render(f:&mut  Frame<'_,CrosstermBackend<io::Stdout>>, app: App) {
     )
     .split(f.size());
     let offset: u16;
-    let mut lines:Vec<&str> = app.input.split("\n").collect();
-    lines.push("");
-    lines.push("");
+    let lines = lines(app.input);
     if lines.len() > chunks[0].height as usize {
         offset = lines.len() as u16 - chunks[0].height;
     } else {
         offset = 0
     }
     let input = Paragraph::new(app.input.as_ref()).scroll((offset,0)).block(Block::default().borders(Borders::ALL));
-    let command = Paragraph::new(vec![Spans::from(Span::raw(app.command))]).style(Style::default()).block(Block::default().borders(Borders::ALL).title("Command"));
+    let command = Paragraph::new(vec![Spans::from(Span::raw(app.command))]).style(Style::default()).block(Block::default().borders(Borders::ALL).title("Mode"));
     f.render_widget(input,chunks[0]);
     f.render_widget(command, chunks[1]);
+    let mut x:u16 = 0;
+    let mut y:u16 = 0;
+    for i in 0..*edit_cursor {
+        if i< app.input.len(){ 
+        x += 1;
+        if &app.input.chars().collect::<Vec<char>>()[i] == &'\n' {
+            x = 0;
+            y += 1;
+        }  
+    } else {
+        *edit_cursor -= 1;
+    }
+    }
+    f.set_cursor(chunks[0].x + x + 1, y + chunks[0].y + 1);
 }
 fn get_mode(command: &String) -> Mode {
    //println!("{:?}",command);
@@ -104,4 +116,10 @@ fn get_mode(command: &String) -> Mode {
         "q" | "quit" => Mode::Quit,
         _ => Mode::Error
     };
+}
+fn lines<'a>(input:&'a String) -> Vec<&'a str> {
+    let mut lines:Vec<&str> = input.split("\n").collect();
+    lines.push("");
+    lines.push("");
+    lines
 }
